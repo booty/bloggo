416cebdf76ad327e17e62ceb8cfc04ff67e40dc0
John Rose <john.rose@upstart.com>
2021-12-30 15:34:24 -0500

    Initial commit; display lorem ipsum posts.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,120 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+class PostIndex extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  displayPost = (e) => {
+    e.preventDefault();
+    const postId = e.currentTarget.dataset.id;
+    console.log("hey, let's show post " + postId);
+    this.props.handlePostSelected(postId);
+    console.log("bye!");
+  }
+
+  render() {
+    const postItems = this.props.posts.map((post) => (
+      <li key={post.id}>
+        <a data-id={post.id} onClick={this.displayPost}>
+          {post.title}
         </a>
-      </header>
-    </div>
-  );
+      </li>
+    ));
+
+    return (
+      <div id="postIndex" className="contentBox">
+        <p>Index of posts</p>
+        <ul>{postItems}</ul>
+      </div>
+    );
+  }
+}
+
+class PostContainer extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    let content = null;
+
+    console.log(typeof(this.props.selectedPost));
+    if (typeof(this.props.selectedPost) === 'undefined') {
+      content = (
+        <p>no post selected</p>
+      )
+    }
+    else {
+      content = (
+        <div>
+          <h1>{this.props.selectedPost.title}</h1>
+          <p>{this.props.selectedPost.body}</p>
+        </div>
+      )
+    }
+    return (
+      <div id="postContainer" className="contentBox">
+        {content}
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handlePostSelected = this.handlePostSelected.bind(this);
+    this.state = {selectedPostId: null};
+  }
+
+  handlePostSelected(selectedPostId) {
+    let selectedPost = this.props.data.posts.filter((x) => {
+      let fuckyou = (x.id === selectedPostId);
+      // console.log("hey does " + x.id + " == " + selectedPostId + "? a:" + fuckyou);
+      return fuckyou;
+    })[0];
+    this.setState({selectedPostId: selectedPostId, selectedPost: selectedPost});
+    console.log("selectedPostId is now:" + this.state.selectedPostId);
+    console.log("selectedPost is now:" + this.state.selectedPost);
+  }
+
+  render() {
+    return (
+      <div id="App">
+        <PostIndex
+          posts={this.props.data.posts}
+          handlePostSelected={this.handlePostSelected}
+        />
+        <PostContainer
+          selectedPost={this.state.selectedPost}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,9 +4,59 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const data = {
+  posts: [
+    {
+      title: "Post 1 Title",
+      body:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nulla lectus, malesuada quis nisi at, efficitur aliquam nulla. Donec et lorem in metus efficitur euismod eu sed diam.",
+      id: "6442",
+      comments: [
+        {
+          body:
+            "Aliquam est nisl, porttitor sed dictum quis, semper vel tortor",
+          user: "Fred",
+          id: "783432"
+        },
+        {
+          body:
+            "Maecenas efficitur tellus nec dui porttitor elementum. Vestibulum quis leo a turpis dapibus placerat. Phasellus accumsan quam pellentesque",
+          user: "Dingus",
+          id: "86323"
+        }
+      ]
+    },
+    {
+      title: "Post 2 Title",
+      body:
+        "Nullam tristique id velit vitae suscipit. Vivamus quis scelerisque mauris. Nulla et nisi tempor, vestibulum odio eget, ultricies nunc. Phasellus faucibus neque diam, ornare pulvinar dolor interdum at.",
+      id: "9082",
+      comments: [
+        {
+          body:
+            "Tiam facilisis nibh a ex tristique maximus. Nulla ullamcorper tempor dolor, in sodales dui placerat et. Praesent quis magna tellus",
+          user: "Bozo",
+          id: "783432"
+        },
+        {
+          body:
+            "Sed eu nisi tincidunt, euismod dolor ac, rutrum nisl. Donec eget mi sed eros tempor hendrerit vel vel ipsum.",
+          user: "Dingus",
+          id: "86323"
+        },
+        {
+          body: "Curabitur vel turpis.",
+          user: "Megatron",
+          id: "99023"
+        }
+      ]
+    }
+  ]
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App data={data}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
